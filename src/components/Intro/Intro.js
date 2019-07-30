f74d451ef93008b2d08a9f1ae4fd657693d244a0
Wang Weijing <you@example.com>
2019-07-30 23:49:42 +1000

    intro component add by using GSAP

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import '../Intro/Intro.css'
+import TweenMax from 'gsap';
+// import { TweenMax } from 'react-gsap';
+import React from 'react'
+import '../Intro/Intro.css';
 
 
-class Intro extends React.Component{
-    constructor(props){
+
+class Intro extends React.Component {
+    constructor(props) {
         super(props);
-        this.myElement = null;
-        
+        this.introElement = null;//reference to the DOM node
+        this.introTween = null; //reference to the animation    
     }
-    render(){
-    return (
-        <div >
-          
-        </div>
-        
-    );
+
+
+
+    componentDidMount(i) {
+        const random = (min, max) => {
+            return (
+                (Math.random() * (max - min)) + min
+            );
+        }
+        this.introTween = TweenMax.to(this.introElement, 2.5, {
+            opacity: 0,
+            x: random(-500, 500),
+            y: random(-500, 500),
+            z: random(-500, 500),
+            scale: .1,
+            delay: i * .02,
+            yoyo: true,
+            repeat: -1,
+            repeatDelay: 10
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div className="box" >
+                    <p ref={div => this.introElement = div}>Hi I'm Jamie</p>
+                </div>
+            </div>
+        );
     }
 }
 export default Intro;
--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ const App = ()=> {
     <div >
       <Navbar />
       <Logo />
-      <Intro />
+      <Intro/>
     </div>
   );
 }
