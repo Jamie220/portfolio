3dd0447600b80b25bcb2ac4d85a07ba4228abd91
Wang Weijing <you@example.com>
2019-08-01 23:31:31 +1000

    onClick button

--- src/components/ResumeButton/ResumeButton.js
+++ src/components/ResumeButton/ResumeButton.js
@@ -7,7 +7,7 @@ const ResumeButton = () => {
         <div className ="text-center">
             <div className="btn">
                 <Tilt className="Tilt" options={{ max: 35 }} style={{ height: 100, width: 100 }} >
-                    <div className="Tilt-inner"><button type="button" className="btn btn-secondary">Resume</button></div>
+                    <div className="Tilt-inner"><button onClick="" type="button" className="btn btn-secondary">Resume</button></div>
                 </Tilt>
             </div>
         </div>
