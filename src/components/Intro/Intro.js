a18042715f5775e1baab7402c64268c7231350b9
Jamie220 <jamiewang220@gmail.com>
2019-08-05 16:49:15 +1000

    restyle home page

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,12 +5,16 @@ import '../Intro/Intro.css';
 const Intro = () => {
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                <main role="main" className="text-center inner cover">
-                    <h1 className="cover-heading animated animatedFadeInUp fadeInUp">Hi, I'm Jamie</h1>
-                    <p className="lead animated animatedFadeInUp fadeInUp">I am a front end developer.</p>
-                </main>
-            </div>
+            <header className="masthead">
+                <div className="container h-100">
+                    <main role="main" className="row h-100 align-items-center inner cover">
+                        <div className="col-12 text-center">
+                        <h1 className="cover-heading animated animatedFadeInUp fadeInUp">Hi, I'm Jamie</h1>
+                        <p className="lead animated animatedFadeInUp fadeInUp">I am a front end developer.</p>
+                        </div>
+                    </main>
+                </div>
+            </header>
         </>
     );
 }
