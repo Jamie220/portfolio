ee7e16fff20438aeef131f579d17320e0cd10ad8
Wang Weijing <you@example.com>
2019-08-05 23:34:25 +1000

    adding skills icon

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from '../components/Navbar/Navbar';
-import Logo from '../components/Logo/Logo'
-import Intro from '../components/Intro/Intro'
-import About from '../components/About/About'
+import Logo from '../components/Logo/Logo';
+import Intro from '../components/Intro/Intro';
+import About from '../components/About/About';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 
 const App = () => {
+  library.add(fab, faCheckSquare, faCoffee)
   return (
     <div>
       
