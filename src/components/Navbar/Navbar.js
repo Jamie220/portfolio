3d466451d873aa21c38fb4a10051d6dfaf1cd250
Jamie220 <jamiewang220@gmail.com>
2019-08-06 11:33:15 +1000

    change navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className='navbar sticky-top d-flex justify-content-sm-end'>     
         <ul className ='nav'>
           <li className='nav-itme'><a className='nav-link active' href='#about'>About</a></li>
-          <li className='nav-itme'><a className='nav-link active' href='#!'>Work</a></li>
+          <li className='nav-itme'><a className='nav-link active' href='#!'>Projects</a></li>
           <li className='nav-itme'><a className='nav-link active' href='#!'>Contact</a></li>
         </ul>
       </nav>
