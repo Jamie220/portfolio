4b67af3a96be06a63d0b1c5aac93589303bb16f2
Jamie220 <jamiewang220@gmail.com>
2019-11-20 15:28:18 +1100

    update about section

--- src/myInformation.js
+++ src/myInformation.js
@@ -10,11 +10,11 @@ export const mainData = {
 export const aboutData = {
     img: './images/portfolio_pic.jpg',
     paragraphOne:
-    "I'm a professional and passionate JavaScript Developer focused on efficiency and constant learning.",
+    "I'm the Front-End Developer from China live in Melbroune.",
   paragraphTwo:
-    '',
+    'I have serious passion for front end development, UI effects, animations and creating intuitive, dynamic user experiences.',
   paragraphThree:
-    'I would love to set up an introduction call to discuss how I could help your business and make your goals happen.',
+    "Let's create something incredible!",
   resume: 'https://drive.google.com/file/d/1y1ESNrxklNJxIcoS86D5fRoIyEHAs_is/view?usp=sharing', // if no resume, the button will not show up
 };
 
