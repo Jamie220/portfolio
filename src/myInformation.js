4e4b7fa6478cd2ef858820ddf6388ea8471cf6bb
Jamie220 <jamiewang220@gmail.com>
2019-11-21 11:55:54 +1100

    update web link

--- src/myInformation.js
+++ src/myInformation.js
@@ -32,7 +32,7 @@ export const projectsData = [
     title:'Face Recognition App',
     info: "A full-stack web application using the MERN stack, this app allows you to identify the detection of faces in any image supplied. This was achieved thanks to Clarifai's artificial intelligence API.",
     info2: 'I used React, Node, Express.js and MongoDB in order to create this web application.',
-    url: 'https://www.google.com.au/',
+    url: 'https://zen-bardeen-4a49a0.netlify.com/',
     repo: 'https://github.com/Jamie220/React-smartBrain',
     img:'./images/smartBrain.png',
   },
